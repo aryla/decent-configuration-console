af062579adb0329633623f09d659c22e1fa1bb06
Arttu Ylä-Outinen <7724847+aryla@users.noreply.github.com>
2025-12-14 19:13:54 +0200

    drop SlotProxy

--- util.py
+++ util.py
@@ -1,29 +1,7 @@
 import functools
 from typing import Any, Callable
 
-from PySide6.QtCore import QMetaObject, QObject, Qt, QTimer, Slot
-
-
-def SlotProxy[T](target: T) -> T:
-    class SlotProxy:
-        def __init__(self, target):
-            for key in dir(target):
-                value = getattr(target, key)
-                if callable(value) and hasattr(value, '_slots'):
-
-                    def helper(key):
-                        def wrapper(*args, **kwargs):
-                            return QMetaObject.invokeMethod(
-                                target, key, Qt.ConnectionType.QueuedConnection, *args, **kwargs
-                            )
-
-                        return wrapper
-
-                    wrapper = helper(key)
-                    functools.update_wrapper(wrapper, value)
-                    setattr(self, key, wrapper)
-
-    return SlotProxy(target)  # pyright: ignore[reportReturnType]
+from PySide6.QtCore import QObject, QTimer, Slot
 
 
 def throttle_key(key_selector: Callable):
